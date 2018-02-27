f967f49d42bb4b88059a897f468ae62408c1f94b
chenaoh <cdhenao@gmail.com>
2018-02-27 09:02:05 -0500

    otro commit desde el segundo

--- PruebaGit/src/Aplicacion.java
+++ PruebaGit/src/Aplicacion.java
@@ -5,7 +5,7 @@ public class Aplicacion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JOptionPane.showMessageDialog(null,"Henao");
-		System.out.println("Este es un nuevo cambio");
+		System.out.println("Este es un nuevo cambio, diferente");
 	}
 
 }
